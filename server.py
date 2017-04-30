0e23020f148b37e32356ef4fbf293b7accd5a1b1
Adam Sherwin <sherwial@clarkson.edu>
2017-04-30 15:24:32 -0400

    asdsa

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import cv2 as cv
 from flask import Flask
 from flask import send_file
 cap = cv.VideoCapture(0)
+cap.set()
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,12 +13,10 @@ def hello_world():
 
 @app.route('/take')
 def take_pic():
+    cap = cv.VideoCapture(0)
     ret,frame = cap.read()
-    cv.imwrite('image.png',frame) 
+    cv.imwrite('image.png',frame)
+    cap.release()
     return "took image"
 if __name__ == "__main__":
-
-
-
-
     app.run(host='0.0.0.0')
