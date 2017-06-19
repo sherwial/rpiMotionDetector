b10e1966195a72a632818dbacb2519d8fb91e283
Adam Sherwin <adamsherwin@Adams-MBP.home>
2017-06-19 18:32:21 -0400

    First stream intro

--- server/run.py
+++ server/run.py
@@ -2,7 +2,9 @@
 import socket
 from flask import Flask
 from flask import send_file,render_template
+from flask import Response
 import threading
+import cv2
 
 # ip = socket.gethostbyname(socket.gethostname())
 
@@ -11,6 +13,7 @@ class Server(threading.Thread):
         self.observs = obs
         threading.Thread.__init__(self)
         self.app = Flask(__name__)
+        self.cam = picamobj
 
         @self.app.route('/')
         def observations():
@@ -22,6 +25,21 @@ class Server(threading.Thread):
                 return send_file('../'+filename+'.jpeg')
             except:
                 return "Image outdated"
+
+        @self.app.route('/video')
+        def video_feed():
+            return Response(self.gen(self.cam),
+                            mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+        def gen(self, camera):
+            while True:
+                frame = camera.getImage()
+                ret, jpeg = cv2.imencode('.jpg', frame)
+                bytes = jpeg.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
         # @self.app.route('/take')
         # def take_pic():
         #     cap = cv.VideoCapture(0)
